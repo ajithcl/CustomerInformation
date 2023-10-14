31f9b7b78dd6b2d896ce53101e9231cbaf34e8cb
Ajith kumar CL <ajith@Ajiths-MacBook-Pro.local>
2023-10-14 23:49:04 +0530

    Customer object class file created

---
+++ Angular/CustomerInformation/src/app/models/customer.ts
@@ -0,0 +1,20 @@
+export class Customer {
+    CustNum : number = 0;                        
+    Country : string = "";                          
+    Name : string = "";
+    Address : string = "";
+    Address2 : string = "";
+    City : string = "";
+    State : string = "";
+    PostalCode : string = "";
+    Contact : string = "";
+    Phone : string = "";
+    SalesRep : string = "";
+    CreditLimit : number = 0;                      
+    Balance : number = 0;                         
+    Terms : string = "";                           
+    Discount : number = 0;                      
+    Comments : string = "";
+    Fax : string = "";
+    EmailAddress : string = ""
+}
